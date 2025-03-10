9373106cef7a661217814f2bd0e049e7d4fe9947
amankmr1207 <amankmr1207@gmail.com>
2025-03-10 10:05:51 -0400

    sending my first repo

---
+++ demo.tf
@@ -0,0 +1,1 @@
+hi this is aman and I want to do the task of the terraform related to the github
---
+++ get-started001/main.tf
@@ -0,0 +1,1 @@
+HI this is the temp file just for testing the connection and the command for github and cli integration .
